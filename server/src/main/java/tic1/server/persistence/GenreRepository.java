f2f657d7876bf1cb90d31d88171d68367ef7af2c
Luis Borrazas <lborrazas@correo.um.edu.uy>
2019-11-06 00:14:57 -0300

    los rest tan andando de lujo

--- server/src/main/java/tic1/server/persistence/GenreRepository.java
+++ server/src/main/java/tic1/server/persistence/GenreRepository.java
@@ -3,6 +3,11 @@ package tic1.server.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tic1.server.entities.Genre;
+import tic1.server.entities.Movie;
+
+import java.util.List;
+
 @Repository
 public interface GenreRepository extends JpaRepository<Genre,Long> {
+    List<Genre> findByGenre(String genre);
 }
