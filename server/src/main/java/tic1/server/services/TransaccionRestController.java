5f50262ef596364fb264c5ea5c3883e58090ce01
jabuarab <alasjan1@gmail.com>
2019-11-15 10:29:29 -0300

    buttons updatred

--- server/src/main/java/tic1/server/services/TransaccionRestController.java
+++ server/src/main/java/tic1/server/services/TransaccionRestController.java
@@ -1,4 +1,13 @@
 package tic1.server.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import tic1.server.business.TransaccionMgr;
+import tic1.server.entities.Transaccion;
+
+@RestController
 public class TransaccionRestController {
+@Autowired
+    TransaccionMgr transaccionMgr;
+
 }
