6a8abd5c947732a08a2b57d0adaa736326c13380
jpalgort <jpalgort@gmail.com>
2019-11-25 20:41:07 -0300

    merge

--- client/src/main/java/tic1/client/ui/adds/AddSalaController.java
+++ client/src/main/java/tic1/client/ui/adds/AddSalaController.java
@@ -8,20 +8,23 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.stage.Stage;
 import org.springframework.stereotype.Controller;
 import tic1.client.models.Sala;
 import tic1.client.models.Seat;
 import tic1.client.models.User;
 import tic1.client.services.SalaRestTemplate;
+import javafx.collections.SetChangeListener.Change;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 @Controller
 public class AddSalaController implements Initializable {
@@ -42,105 +45,6 @@ public class AddSalaController implements Initializable {
             }
         }
     }
-
-    /*private final ObservableSet<Button> selectedLabels = FXCollections.observableSet();
-
-
-    private final int ROWS = 10 ;
-    private final int COLS = 16 ;
-
-    private final Button[][] buttons = new Button[COLS][ROWS];
-
-    private final PseudoClass SELECTED = PseudoClass.getPseudoClass("selected");
-
-    @Override
-    public void start(Stage primaryStage) {
-        GridLocation mouseDownLoc = new GridLocation();
-        GridPane grid = new GridPane();
-        grid.setHgap(5);
-        grid.setVgap(5);
-        grid.setGridLinesVisible(true);
-
-        for (int i = 0; i < COLS; i++) {
-            for (int j = 0; j < ROWS; j++) {
-                addLabel(grid, i, j, mouseDownLoc);
-            }
-        }
-
-        selectedLabels.addListener((Change<? extends Label> change) -> {
-            if (change.wasAdded()) {
-                Label label = change.getElementAdded();
-                label.pseudoClassStateChanged(SELECTED, true);
-            } else if (change.wasRemoved()) {
-                Label label = change.getElementRemoved();
-                label.pseudoClassStateChanged(SELECTED, false);
-            }
-        });
-
-        Scene scene = new Scene(grid, 800, 800);
-        scene.getStylesheets().add("dragging-grid-pane.css");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    private void addLabel(GridPane grid, int col, int row,
-                          GridLocation mouseDownLoc) {
-        Label label = new Label("Cell ["+(col+1)+", "+(row+1)+"]");
-        labels[col][row] = label ;
-
-        grid.add(label, col, row);
-        GridPane.setFillWidth(label, true);
-        GridPane.setHgrow(label, Priority.ALWAYS);
-        GridPane.setVgrow(label, Priority.ALWAYS);
-
-        label.setAlignment(Pos.CENTER);
-        label.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-
-
-        label.setOnDragDetected(event -> {
-            mouseDownLoc.x = col ;
-            mouseDownLoc.y = row ;
-            selectedLabels.clear();
-            selectedLabels.add(label);
-            label.startFullDrag();
-        });
-
-        label.setOnMouseDragEntered(event ->
-                recomputeSelection(mouseDownLoc, col, row));
-
-    }
-
-    private void recomputeSelection(GridLocation mouseDown,
-                                    int x, int y) {
-        Set<Label> newSelection = new HashSet<>();
-        int startX = Math.min(x,  mouseDown.x);
-        int endX = Math.max(x, mouseDown.x);
-        int startY = Math.min(y, mouseDown.y);
-        int endY = Math.max(y, mouseDown.y);
-
-        for (int j = startY; j <= endY; j++) {
-            for (int i = startX; i <= endX; i++) {
-                newSelection.add(labels[i][j]);
-            }
-        }
-
-        // remove anything in selectedLabels
-        // that is not in newSelection:
-        selectedLabels.retainAll(newSelection);
-
-        // add everything from newSelection
-        // to selectedLabels (will not duplicate):
-        selectedLabels.addAll(newSelection);
-    }
-
-    private static class GridLocation {
-        int x, y ;
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}*/
 
     @FXML
     private void createSeat(ActionEvent event) {
